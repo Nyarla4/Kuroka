07f0c15d46143b4cfaeecbf22a956c5c1668bd9c
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2026-04-06 02:05:02 +0900

    로로카 부활 처리 수정, 위치 아직 조정중

--- KurokaCode/Commands/RorokaCmd.cs
+++ KurokaCode/Commands/RorokaCmd.cs
@@ -10,32 +10,29 @@ namespace Kuroka.KurokaCode.Commands;
 
 public static class RorokaCmd
 {
-  public static async Task SummonOrHeal(CombatState combatState, uint? rorokaId, Decimal amount, Player owner)
-  {
-    Creature? roroka = null;
-    
-    if (rorokaId.HasValue)
+    public static async Task SummonOrHeal(CombatState combatState, uint? rorokaId, Decimal amount, Player owner)
     {
-      roroka = combatState.GetCreature(rorokaId.Value);
-    }
+        Creature? roroka = null;
 
-    if (roroka == null || roroka.IsDead)
-    {
-      var roroModel = ModelDb.Get<Roroka>();
-      
-      var clonedRoroka = (Roroka)roroModel.MutableClone();
-      
-      roroka = await CreatureCmd.Add(clonedRoroka, combatState, CombatSide.Player); 
-        
-      roroka.PetOwner = owner;
-      
-      await PowerCmd.Apply<RorokaPower>(roroka, 1M, null, null);
+        if (rorokaId.HasValue)
+        {
+            roroka = combatState.GetCreature(rorokaId.Value);
+        }
 
-      roroka.CurrentHp = (int)amount;
+        if (roroka == null)
+        {
+            var roroModel = ModelDb.Get<Roroka>();
+            var clonedRoroka = (Roroka)roroModel.MutableClone();
+
+            roroka = await CreatureCmd.Add(clonedRoroka, combatState, CombatSide.Player);
+            roroka.PetOwner = owner;
+
+            await PowerCmd.Apply<RorokaPower>(roroka, 1M, null, null);
+            roroka.CurrentHp = (int)amount;
+        }
+        else
+        {
+            await CreatureCmd.Heal(roroka, amount);
+        }
     }
-    else
-    {
-      await CreatureCmd.Heal(roroka, amount);      
-    }
-  }
 }
