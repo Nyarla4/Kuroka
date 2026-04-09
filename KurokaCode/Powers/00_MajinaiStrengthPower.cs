10b2d481762be54d43d4346de4aee705e9b69b8f
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2026-04-09 22:05:02 +0900

    망상인자 설명 추가, 주문강화 추가
    근데 주문강화를 못얻고있어

---
+++ KurokaCode/Powers/00_MajinaiStrengthPower.cs
@@ -0,0 +1,35 @@
+﻿using Godot;
+using MegaCrit.Sts2.Core.Combat;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.Entities.Powers;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Logging;
+using MegaCrit.Sts2.Core.Models;
+using MegaCrit.Sts2.Core.ValueProps;
+using Logger = MegaCrit.Sts2.Core.Logging.Logger;
+
+namespace Kuroka.KurokaCode.Powers;
+
+public class MajinaiStrengthPower : KurokaPower
+{
+    
+    public override PowerType Type => PowerType.Buff;
+
+    public override PowerStackType StackType => PowerStackType.Counter;
+
+    public static int GetMajinaiedCreatures(IReadOnlyList<Creature> creatures)
+    {
+        return creatures.Where(c => c.GetPower<MajinaiPower>() != null).ToList().Count;
+    }
+    
+    public override Decimal ModifyDamageAdditive(
+        Creature? target,
+        Decimal amount,
+        ValueProp props,
+        Creature? dealer,
+        CardModel? cardSource)
+    {
+        return this.Owner != dealer || !props.IsPoweredAttack() ? 0M : GetMajinaiedCreatures(CombatState.GetCreaturesOnSide(CombatSide.Enemy));
+    }
+}
