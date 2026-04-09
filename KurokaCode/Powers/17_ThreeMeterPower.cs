fc0b65a62c5f8c936f1a14b472d44c9ea47364c6
Nyarla4 <31647681+Nyarla4@users.noreply.github.com>
2026-04-09 16:18:19 +0900

    Update 17_ThreeMeterPower.cs
    턴 시작시 소멸 처리

--- KurokaCode/Powers/17_ThreeMeterPower.cs
+++ KurokaCode/Powers/17_ThreeMeterPower.cs
@@ -6,4 +6,17 @@ public class ThreeMeterPower : KurokaPower
 {
     public override PowerType Type => PowerType.Buff;
     public override PowerStackType StackType => PowerStackType.Counter;
+    
+    public override async Task AfterPlayerTurnStart(PlayerChoiceContext choiceContext, Player player)
+    {
+        if (this.Owner.IsDead)
+        {
+            await base.AfterPlayerTurnStart(choiceContext, player);
+            return;
+        }
+
+        await PowerCmd.Remove<ThreeMeterPower>(this.Owner);
+        await base.AfterPlayerTurnStart(choiceContext, player);
+    }
+    // 피격시 찾아서 것 DelusionFactorPower Amount * this.Amount 반격 처리 할 것
 }
